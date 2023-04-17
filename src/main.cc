a95e3bfc37896f7551adb51df420ccc905a20a43
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2023-04-17 20:13:07 +0530

    vbcvbcvb

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,36 @@
 
-
 // Auto Genrated C++ file by newton CLI
 #include "../include/crow_all.h"
+void send_file(crow::response&res,const std::string&file_name,const std::string&content_type)
+{
+       std::ifstream in("public/"+file_name,std::ifstream::in);
+        if(in)
+        {
+            std::ostringstream contens;
+            contens<<in.rdbuf();
+            in.close();
+            res.set_header("Content-Type",content_type);
+            res.write(contens.str());
+        }else
+        {
+            res.code=404;
+            res.write("Not found!\n");
+        };
+
+        res.end();
+};
 
 int main(int argc, char *argv[])
 {
+  
     crow::SimpleApp app;
     CROW_ROUTE(app, "/")
-    ([]()
-     { return "<h1>Chat Server...</h1>"; });
+    ([](const crow::request&req, crow::response&res)
+     
+     {
+        send_file(res,"index.html","plain-text");
+     }
+     );
     char *port = getenv("PORT");
     uint16_t iport = static_cast<uint16_t>(port ? std::stoi(port) : 8080);
     app.port(iport).multithreaded().run();
